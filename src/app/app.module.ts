f1d7eb7420ae53dd490bec56b7f0d825e4d3cf09
Hugo <hugo_val28@hotmail.com>
2018-05-19 17:42:58 -0600

    added project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
+// Services
+import { AlertService, AuthenticationService } from './_services/index';
+
+// Guards
+import { AuthGuard } from './_guards/index';
+
+// Directives
+import { AlertComponent } from './_directives/index';
+
+// Helpers
+import { JwtInterceptor } from './_helpers/index';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
